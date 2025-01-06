f7bfc37fb13e33eb16d03078f8f8d07a7f82f3d0
YoulimKim <kimyoulim1016@gmail.com>
2025-01-06 22:11:01 +0900

    jump-game-ii => Test case 42번에서 막힘

---
+++ src/main/java/com/lima/studyalgorithm/StudyAlgorithmApplication.java
@@ -0,0 +1,13 @@
+package com.lima.studyalgorithm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyAlgorithmApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(StudyAlgorithmApplication.class, args);
+  }
+
+}
---
+++ src/main/java/com/lima/studyalgorithm/flab20250106/Solution03.java
@@ -0,0 +1,29 @@
+package com.lima.studyalgorithm.flab20250106;
+
+/*
+https://leetcode.com/problems/first-missing-positive/description/
+정렬되지 않은 배열 nums가 주어졌을 때, 정수 양수 중 배열에 포함되어있지 않은 최소 숫자를 리턴하세요.
+알고리즘은 반드시 O(n)의 시간복잡도를 가져야하며 constant 공간을 추가로 사용해야합니다.
+예제 1:
+입력: nums = [1, 2, 0]
+출력: 3
+예제 2:
+입력: nums = [3, 4, -1, 1]
+출력: 2
+예제 3:
+입력: nums = [7, 8, 9, 11, 12]
+출력: 1
+예제 4:
+입력: nums = [5, 2, 3, 7, 6, 4, 1, 9]
+출력: 8
+제약사항:
+1 <= nums.length <= 10^5
+-2^31 <= nums[i] <= 2^31 - 1
+구현할 method:
+public int firstMissingPositive(int[] nums) {
+	// implementation
+}
+ */
+public class Solution03 {
+
+}
